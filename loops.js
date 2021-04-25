cd133e0b567faffb4415e16aa5a41410e872a25f
NYLA <nylaschoolconnect@gmail.com>
2021-04-25 18:27:30 -0500

    Updated loop document

--- loops.js
+++ loops.js
@@ -13,3 +13,92 @@ for (let i = 0; i < cats.length; i++) {
 }
 
 console.log(info)
+
+
+
+
+// loop with the comma remove from the end of the sentence
+
+const animals = ['Bill', 'Jeff', 'Pete', 'Biggles', 'Jasmin'];
+
+let information = ' '
+
+const paragraph = document.querySelector('p');
+
+for (let i = 0; i < animals.length; i++) {
+
+    if (i === animals.length - 1) {
+
+        information += 'and ' + animals[i] + '.'
+    } else {
+        information += animals[i] + ', ';
+
+    }
+
+
+}
+
+console.log(information)
+
+
+// Exiting loops with break
+
+// const contacts = ['Chris:2232322', 'Sarah:3453456', 'Bill:7654322', 'Mary:9998769', 'Dianne:9384975'];
+// const para = document.querySelector('p');
+// const input = document.querySelector('input');
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', function() {
+//     let searchName = input.value.toLowerCase();
+//     input.value = '';
+//     input.focus();
+//     for (let i = 0; i < contacts.length; i++) {
+//         let splitContact = contacts[i].split(':');
+//         if (splitContact[0].toLowerCase() === searchName) {
+//             para.textContent = splitContact[0] + '\'s number is ' + splitContact[1] + '.';
+//             break;
+//         } else if (i === contacts.length - 1) {
+//             para.textContent = 'Contact not found.';
+//         }
+//     }
+// });
+
+
+// Skipping iterations with continue
+
+// let num = input.value;
+
+// for (let i = 1; i <= num; i++) {
+//     let sqRoot = Math.sqrt(i);
+//     if (Math.floor(sqRoot) !== sqRoot) {
+//         continue;
+//     }
+
+//     para.textContent += i + ' ';
+// }
+
+// Launch countdown!
+
+
+const output = document.querySelector('.output');
+output.innerHTML = '';
+
+let i = 10;
+
+while (i >= 0) {
+    let para = document.createElement('p');
+    if (i === 10) {
+        para.textContent = 'Countdown ' + i;
+    } else if (i === 0) {
+        para.textContent = 'Blast off!';
+    } else {
+        para.textContent = i;
+    }
+
+    output.appendChild(para);
+
+    i--;
+}
+
+
+// LOOPS: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Looping_code#exiting_loops_with_break
